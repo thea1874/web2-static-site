9e4be151fefdff800abe62160506ecd43b66d6b4
Thea Nielsen <thea1874@kea.dk>
2023-02-03 11:50:21 +0100

    new branch

---
+++ js/product.js
@@ -0,0 +1,9 @@
+
+fetch("https://kea-alt-del.dk/t7/api/products/1525")
+.then((Response) => Response.json())
+.then((data) => console.log(data));
+
+function showProduct(product){
+    console.log(product);
+    document.querySelector(".purchaseBox h3").textContent = product.productdisplayname;
+}
