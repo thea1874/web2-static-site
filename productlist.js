e76e4e3a3e1d479bc97ea9a009bc090836ed157a
Thea Nielsen <thea1874@kea.dk>
2023-02-06 15:00:43 +0100

    produktliste med api

---
+++ productlist.js
@@ -0,0 +1,41 @@
+fetch("https://kea-alt-del.dk/t7/api/products")
+    .then(res=>res.json())
+    .then(data=>showProducts(data))
+
+
+
+    function showProducts(products){
+//looper og kalder 
+products.forEach(showProduct)
+    
+    }
+
+    function showProduct (product){
+//fange template 
+const template = document.querySelector("#smallProducttemplate").content;
+//lav kopi 
+const copy =template.cloneNode(true);
+//ændre indhold
+copy.querySelector("h3").textContent = product.productdisplayname;
+
+
+copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp` ; 
+
+
+
+//copy.querySelector("").textContent = product.brandname;
+
+
+if(product.soldout){
+    //produkt udsolgt
+    copy.querySelector("article").classList.add("soldOut")
+}
+
+if(product.onSale){
+    //produkt på sale (dur ikke helt )
+    copy.querySelector("article").classList.add("onSale")
+}
+//appende
+document.querySelector ("main").appendChild(copy);
+
+    }
