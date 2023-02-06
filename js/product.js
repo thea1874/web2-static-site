09cecd6a171a6c1c89257df242a8f06bb59c1ad6
Thea Nielsen <thea1874@kea.dk>
2023-02-06 14:00:36 +0100

    produkter v1 færdig

--- js/product.js
+++ js/product.js
@@ -3,7 +3,12 @@ fetch("https://kea-alt-del.dk/t7/api/products/1525")
 .then((Response) => Response.json())
 .then((data) => showProduct(data));
 
-function showProduct(product){
+function showProduct(product) {
     console.log(product);
     document.querySelector(".purchaseBox h3").textContent = product.productdisplayname;
+    document.querySelector(".purchaseBox .brand").textContent = product.brandname;
+    document.querySelector(
+        "img"
+        ).src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp` ; 
+    
 }
