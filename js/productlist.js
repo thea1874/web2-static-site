f66f74c5dbb9a61b456fc0c2727e506fdf7115ec
Thea Nielsen <thea1874@kea.dk>
2023-02-07 08:53:03 +0100

    flyttet produktliste

--- js/productlist.js
+++ js/productlist.js
@@ -18,9 +18,11 @@ const copy =template.cloneNode(true);
 //ændre indhold
 copy.querySelector("h3").textContent = product.productdisplayname;
 
-
 copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp` ; 
 
+//brand 
+//price 
+//discount 
 
 
 //copy.querySelector("").textContent = product.brandname;
